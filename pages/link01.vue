c67bb35fedc3d478b13c9559c70f5c77576cad79
Satoru <you@example.com>
2023-12-07 14:46:42 +0900

    fix: bug

--- pages/link01.vue
+++ pages/link01.vue
@@ -2,6 +2,9 @@
   <v-container>
     <vc-config-provider :cesium-path="vcConfig.cesiumPath">
       <vc-viewer>
+        <vc-layer-imagery>
+          <vc-imagery-provider-arcgis> </vc-imagery-provider-arcgis>
+        </vc-layer-imagery>
         <vc-entity :position="[-75.59777, 40.03883]" description="Hello VueCesium">
       <vc-graphics-point ref="point1" color="red" :pixel-size="8"></vc-graphics-point>
     </vc-entity>
@@ -11,7 +14,8 @@
 </template>
 <script setup>
 import { reactive } from "vue";
-import { VcConfigProvider, VcViewer, VcEntity, VcGraphicsPoint } from "vue-cesium";
+import { VcConfigProvider, VcViewer, VcEntity, VcGraphicsPoint, VcLayerImagery,
+  VcImageryProviderArcgis, } from "vue-cesium";
 
 const vcConfig = reactive({
   cesiumPath: "https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js",
