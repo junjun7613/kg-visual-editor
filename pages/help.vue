b30678741deb4a5700f466ccbaf89a13e2e39199
Jun Ogawa <junogawa@dhcp167-142.rall.nii.ac.jp>
2024-04-05 15:29:43 +0900

    add developer info

--- pages/help.vue
+++ pages/help.vue
@@ -2,7 +2,7 @@
     <v-app>
       <v-content>
       <v-container fluid class="mainSearch">
-      <h3 class="searchTitle">エディタの利用方法を解説する動画を公開予定。</h3>
+        <h3 class="searchTitle">エディタの利用方法を解説する動画を公開予定。<br/>開発者：小川潤（国立情報学研究所）・中村覚（東京大学史料編纂所）</h3>
       </v-container>
       </v-content>
     </v-app>
