b61c4c520fe18e8029a7a429347d111b882993ab
Jun Ogawa <junogawa@junnoMacBook-Pro.local>
2024-04-08 00:29:05 +0900

    change help page

--- pages/help.vue
+++ pages/help.vue
@@ -8,16 +8,21 @@
       </v-content>
     </v-app>
     -->
-  <div id="app">
-    <div class="main-container">
-      <p>HIMIKO Editor for Imageは、小川潤（国立情報学研究所）・中村覚（東京大学史料編纂所）<br/>によって開発されたRDFデータ編集エディタです。<br/><br/>このページでは、エディタの使い方に関する各種資料(ブログ記事・解説動画等)を順次公開していきます。</p>
-      <ul class="blog-list">
-        <li class="blog-list-item">
-          <a href="https://junjun7613.hatenablog.com/entry/2024/04/07/154436" target="_blank" class="blog-link">「HIMIKO Editor for Imageの使い方（基礎編）」</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <v-app>
+    <v-container>
+      <div class="main-container">
+        <p>HIMIKO Editor for
+          Imageは、小川潤（国立情報学研究所）・中村覚（東京大学史料編纂所）<br />によって開発されたRDFデータ編集エディタです。<br /><br />このページでは、エディタの使い方に関する各種資料(ブログ記事・解説動画等)を順次公開していきます。
+        </p>
+        <ul class="blog-list">
+          <li class="blog-list-item">
+            <a href="https://junjun7613.hatenablog.com/entry/2024/04/07/154436" target="_blank"
+              class="blog-link">「HIMIKO Editor for Imageの使い方（基礎編）」</a>
+          </li>
+        </ul>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 <style>
 /*
@@ -29,20 +34,24 @@
 }
 */
 .main-container {
-  max-width: 1200px;
+  /*max-width: 1200px;*/
   color: #666;
   margin: 20px auto;
   padding: 20px;
   font-weight: bold;
 }
+
 .blog-list {
   margin-top: 50px;
-  list-style-type: none; /* リストマーカーを削除 */
-  padding: 0; /* デフォルトのパディングを削除 */
+  list-style-type: none;
+  /* リストマーカーを削除 */
+  padding: 0;
+  /* デフォルトのパディングを削除 */
 }
 
 .blog-list-item {
-  margin-bottom: 10px; /* リストアイテム間の余白 */
+  margin-bottom: 10px;
+  /* リストアイテム間の余白 */
   font-size: 18px;
 }
 
